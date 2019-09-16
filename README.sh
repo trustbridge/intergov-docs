0f1648808d7905018fa81156e1aef366b2d35b12
Chris Gough <christopher.d.gough@gmail.com>
2019-09-17 09:14:59 +1000

    README.sh - how to build docs

---
+++ README.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script builds the docs.
+# It assumes this working copy (intergov-docs/)
+# is adjacent to a copy of the intergov codebase
+#
+# This system is a kludge around a problem with the readthedocs setup.
+#
+# Do not edit the files in this repo directly,
+# your changes will get clobbered!
+#
+CODE_SRC_DIR=../intergov
+DOC_BUILD_DIR=$CODE_SRC_DIR/docs
+DOC_SRC_DIR=$(pwd)
+
+cd $DOC_BUILD_DIR
+make html
+# TODO: delte absent files first
+# for file in $(ls) that is not in $KEEP_LIST,
+# if it is not in ls ...
+cp -r _build/html/* $DOC_SRC_DIR
